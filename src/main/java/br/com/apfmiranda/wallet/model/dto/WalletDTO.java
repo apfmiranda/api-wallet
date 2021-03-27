feeb03eb8e6f9522fbc7e2d1d8a7ef98093e47f1
apfmiranda <alexandre.miranda@gmail.com>
2021-03-26 23:43:02 -0300

    Alterações de mensagens e status de retorno

--- src/main/java/br/com/apfmiranda/wallet/model/dto/WalletDTO.java
+++ src/main/java/br/com/apfmiranda/wallet/model/dto/WalletDTO.java
@@ -8,6 +8,7 @@ import org.hibernate.validator.constraints.Length;
 
 import br.com.apfmiranda.wallet.model.entity.Wallet;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author Alexandre Pires Ferrerira de Miranda <alexandre.pires@nagem.com.br>
@@ -16,13 +17,14 @@ import lombok.Data;
  *
  */
 @Data
+@NoArgsConstructor
 public class WalletDTO {
 	
 	private Long id;
-	@Length(min = 3)
-	@NotNull
+	@Length(min = 3, message = "O nome deve conter no minimo 3 caracteres")
+	@NotNull(message = "Nome não pode ser nulo")
 	private String name;
-	@NotNull
+	@NotNull(message = "Insira um valor para carteira")
 	private BigDecimal value;
 	
 	public WalletDTO(Wallet wallet) {
